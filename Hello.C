209afac59424481e705bd88e6f7d1fe86a7f5a8a
Lorelei Trimberger <ltrimberger2@huskers.unl.edu>
2020-08-15 19:56:29 +0000

    inital commit

---
+++ Hello.C
@@ -0,0 +1,17 @@
+/**
+ * Author Lorelei Trimberger
+ * Date: 2020
+ * 
+ * Hello world program
+ */
+ 
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+ int main(int argc, char **argv) {
+     
+     printf("Hello World!\n");
+     
+     return 0;
+ }
+ 
