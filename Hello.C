c3860d3d3c1ff24510558cb38fa9447b448e798a
Lorelei Trimberger <ltrimberger2@huskers.unl.edu>
2020-08-15 20:15:04 +0000

    changed message to my name

--- Hello.C
+++ Hello.C
@@ -1,17 +1,16 @@
 /**
  * Author Lorelei Trimberger
  * Date: 2020
- * 
+ *
  * Hello world program
  */
- 
+
  #include <stdlib.h>
  #include <stdio.h>
- 
+
  int main(int argc, char **argv) {
-     
-     printf("Hello World!\n");
-     
+
+     printf("Lorelei\n");
+
      return 0;
  }
- 
